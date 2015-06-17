1a336526877547715bc936c996b3dc763b2e0621
martin <martin@debian.polytope.org>
2015-06-17 14:09:59 +0100

    restructure, add context senstive menus

---
+++ PyMothDB/dialogs.py
@@ -0,0 +1,132 @@
+#from http://www.effbot.org/tkinterbook/tkinter-dialog-windows.htm
+
+from Tkinter import *
+
+class MyDialog(Toplevel):
+
+    def __init__(self, parent, title = None):
+
+        Toplevel.__init__(self, parent)
+        self.transient(parent)
+
+        if title:
+            self.title(title)
+
+        self.parent = parent
+
+        self.result = None
+
+        body = Frame(self)
+        self.initial_focus = self.body(body)
+        body.pack(padx=5, pady=5)
+
+        self.buttonbox()
+
+        self.grab_set()
+
+        if not self.initial_focus:
+            self.initial_focus = self
+
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+
+        self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
+                                  parent.winfo_rooty()+50))
+
+        self.initial_focus.focus_set()
+
+        self.wait_window(self)
+
+    #
+    # construction hooks
+
+    def body(self, master):
+        # create dialog body.  return widget that should have
+        # initial focus.  this method should be overridden
+
+        pass
+
+    def buttonbox(self):
+        # add standard button box. override if you don't want the
+        # standard buttons
+
+        box = Frame(self)
+
+        w = Button(box, text="OK", width=10, command=self.ok, default=ACTIVE)
+        w.pack(side=LEFT, padx=5, pady=5)
+        w = Button(box, text="Cancel", width=10, command=self.cancel)
+        w.pack(side=LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
+
+    #
+    # standard button semantics
+
+    def ok(self, event=None):
+
+        if not self.validate():
+            self.initial_focus.focus_set() # put focus back
+            return
+
+        self.withdraw()
+        self.update_idletasks()
+
+        self.apply()
+
+        self.cancel()
+
+    def cancel(self, event=None):
+
+        # put focus back to the parent window
+        self.parent.focus_set()
+        self.destroy()
+
+    #
+    # command hooks
+
+    def validate(self):
+
+        return 1 # override
+
+    def apply(self):
+
+        pass # override
+    
+    
+class ConnectDlg(MyDialog):
+    
+    def apply(self):
+        hostname = self.hstNameInp.get()
+        dbName = self.dbNameInp.get()
+        userName = self.usrNameInp.get()
+        passwd = self.psswdInp.get()
+        
+        self.result = hostname,dbName,userName,passwd
+        
+    def body(self,master):        
+        self.hstNameLbl = Label(master,text="Hostname:")
+        self.hstNameLbl.grid(row=0,column=0,sticky=N+S+E+W)
+        self.hstNameInp = Entry(master)
+        self.hstNameInp.insert(0,"localhost")
+        self.hstNameInp.grid(row=0,column=1,sticky=N+S+E+W)
+        
+        self.dbNameLbl = Label(master,text="Database:")
+        self.dbNameLbl.grid(row=1,column=0,sticky=N+S+E+W)       
+        self.dbNameInp = Entry(master)
+        self.dbNameInp.insert(0,"mothrecs")
+        self.dbNameInp.grid(row=1,column=1,sticky=N+S+E+W)
+        
+        self.usrNameLbl = Label(master,text="User Name:")
+        self.usrNameLbl.grid(row=2,column=0,sticky=N+S+E+W)
+        self.usrNameInp = Entry(master)
+        self.usrNameInp.insert(0,"martin")
+        self.usrNameInp.grid(row=2,column=1,sticky=N+S+E+W)
+        
+        self.psswdLbl = Label(master,text="Password:")
+        self.psswdLbl.grid(row=3,column=0,sticky=N+S+E+W)
+        self.psswdInp = Entry(master,show="*")
+        self.psswdInp.grid(row=3,column=1,sticky=N+S+E+W) 
+        
+        return self.psswdInp#initial focus
--- PyMothDB/tk1.py
+++ PyMothDB/tk1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python      
 import Tkinter as tk   
-import MyDialog as d 
+import dialogs as d 
 import mysql.connector  
 from mysql.connector import Error
 import tkMessageBox
@@ -10,22 +10,35 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.parent = master
         self.connected = False
+        
+        
+        
         self.constructUI()
         
     def constructUI(self):
         self.parent.title("Moth records database")  
-        menuBar = tk.Menu(self.parent)
-        self.parent.config(menu=menuBar) 
+        self.menuBar = tk.Menu(self.parent)
+        self.parent.config(menu=self.menuBar) 
         
-        fileMenu = tk.Menu(menuBar)
-        fileMenu.add_command(label="Connect...", command=self.onConnectCmd)
-        fileMenu.add_command(label="Quit", command=self.onExit)
+        self.fileMenu = tk.Menu(self.menuBar)
+        self.fileMenu.add_command(label="Connect...", command=self.onConnectCmd)
+        self.fileMenu.add_command(label="Quit", command=self.onExit)
         
-        menuBar.add_cascade(label="File", menu=fileMenu)
+        self.addRecMenu = tk.Menu(self.menuBar)
+        self.addRecMenu.add_command(label = "Add record event...")
+        self.addRecMenu.add_command(label = "Add record data...")
+        self.addRecMenu.add_command(label = "Add taxon data...")
+        
+        self.queryMenu = tk.Menu(self.menuBar)
+        self.queryMenu.add_command(label="Run a query...")
+                
+        self.menuBar.add_cascade(label="File", menu=self.fileMenu)                
+        self.menuBar.add_cascade(label="Add records", menu=self.addRecMenu, state=tk.DISABLED)
+        self.menuBar.add_cascade(label="Query", menu=self.queryMenu, state=tk.DISABLED)
                 
         
     def onConnectCmd(self):
-        dlg = ConnectDlg(self)
+        dlg = d.ConnectDlg(self)
         params = dlg.result
         #print params
         
@@ -35,6 +48,9 @@ class Application(tk.Frame):
                               host=params[0],
                               database=params[1])
             self.connected = True
+            #activate add and query records - indexing start at 1 not 0!
+            self.menuBar.entryconfig(2, state=tk.NORMAL)
+            self.menuBar.entryconfig(3, state=tk.NORMAL)
             
             #curA = self.cnx.cursor()
 
@@ -57,41 +73,7 @@ class Application(tk.Frame):
     def onExit(self):
         self.quit()                
 
-class ConnectDlg(d.MyDialog):
-    
-    def apply(self):
-        hostname = self.hstNameInp.get()
-        dbName = self.dbNameInp.get()
-        userName = self.usrNameInp.get()
-        passwd = self.psswdInp.get()
-        
-        self.result = hostname,dbName,userName,passwd
-        
-    def body(self,master):        
-        self.hstNameLbl = tk.Label(master,text="Hostname:")
-        self.hstNameLbl.grid(row=0,column=0,sticky=tk.N+tk.S+tk.E+tk.W)
-        self.hstNameInp = tk.Entry(master)
-        self.hstNameInp.insert(0,"localhost")
-        self.hstNameInp.grid(row=0,column=1,sticky=tk.N+tk.S+tk.E+tk.W)
-        
-        self.dbNameLbl = tk.Label(master,text="Database:")
-        self.dbNameLbl.grid(row=1,column=0,sticky=tk.N+tk.S+tk.E+tk.W)       
-        self.dbNameInp = tk.Entry(master)
-        self.dbNameInp.insert(0,"mothrecs")
-        self.dbNameInp.grid(row=1,column=1,sticky=tk.N+tk.S+tk.E+tk.W)
-        
-        self.usrNameLbl = tk.Label(master,text="User Name:")
-        self.usrNameLbl.grid(row=2,column=0,sticky=tk.N+tk.S+tk.E+tk.W)
-        self.usrNameInp = tk.Entry(master)
-        self.usrNameInp.insert(0,"martin")
-        self.usrNameInp.grid(row=2,column=1,sticky=tk.N+tk.S+tk.E+tk.W)
-        
-        self.psswdLbl = tk.Label(master,text="Password:")
-        self.psswdLbl.grid(row=3,column=0,sticky=tk.N+tk.S+tk.E+tk.W)
-        self.psswdInp = tk.Entry(master,show="*")
-        self.psswdInp.grid(row=3,column=1,sticky=tk.N+tk.S+tk.E+tk.W) 
-        
-        return self.psswdInp#initial focus
+
         
         
      
